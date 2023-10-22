c610e4dd0d77bf5a51ad0ffef210ecac393b7aa4
Harsha3199 <harshakoppula3199@gmail.com>
2023-10-22 20:16:23 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,27 @@
+// Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>
+      Contact Me :
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href="koppulaharsha.kumari2021@vitstudent.ac.in">Email</a>
+        </li>
+        <li>
+          <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/k-harsha-150706234/" target="_blank" rel="noopener noreferrer">Linkedin</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong> <a href="https://github.com/Harsha3199" target="_blank" rel="noopener noreferrer">github</a>
+        </li>
+       
+      </ul>
+         </section>
+  );
+};
+
+export default Contact;
